1a4425e24f461852624a696c319c094572b5aefb
Moonfih <victorhugorodriguesguimaraes@gmail.com>
2022-08-15 14:55:50 -0300

    barra lateral em js e exportada pra app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-//import logo from './logo.svg';
-import logo from './assets/Logo Girassol.jpg';
+import logo from './assets/Logo Girassol.png';
 import React, { Fragment } from 'react';
 import './App.css';
 
 import Config from './components/Configuracoes';
+import Bar from './components/Barra lateral';
 
 function App() {
   return (
     <Fragment>
+      <Bar/>
       <Config/>
     </Fragment>
   );
--- src/components/Barra lateral/index.js
+++ src/components/Barra lateral/index.js
@@ -1,48 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css">
-    <title>Barra Lateral</title>
-</head>
-<body>
-    <div class="caixa">
+import React, {Fragment} from "react";
+
+import "./styles.css";
+import perfil from '../../assets/Imagens Perfil/verde escuro.jpg'
+import logo from '../../assets/Logo Girassol.png'
+
+
+export default function Bar(){
+
+
+    return(
+        <Fragment>
+            <div class="caixa">
         <div class="navigation">
             <ul>
-                <img class="logo" src="../../assets/Logo Girassol.png">
+                <img class="logo" src={logo} alt="logo"></img>
                 <h1>Estuder's</h1>
                 <li class="list active">
-                    <a href="#">
+                    <a href="##">
                         <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
                         <span class="title">Início</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="#">
+                    <a href="##">
                         <span class="icon"><ion-icon name="clipboard-outline"></ion-icon></span>
                         <span class="title">Disciplinas</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="#">
+                    <a href="##">
                         <span class="icon"><ion-icon name="calendar-outline"></ion-icon></span>
                         <span class="title">Tarefas e eventos</span>
                     </a>
                 </li>
                 <li class="list">
-                    <a href="#">
+                    <a href="##">
                         <span class="icon"><ion-icon name="settings-outline"></ion-icon></span>
                         <span class="title">Configurações</span>
                     </a>
                 </li>
                 
                 <li class="miniPerfil">
-                    <img class="perfilPic" src="../../assets/Imagens Perfil/verde escuro.jpg">
+                    <img class="perfilPic" src={perfil} alt=""></img>
                     <span class="nome" id="nome">Nome</span>
                     <span class="email" id="email">email@email.com</span>
-                    <a href="#">
+                    <a href="##">
                         <span class="icon-exit"><ion-icon name="log-out-outline"></ion-icon></span>
                     </a>
                 </li>
@@ -52,6 +54,8 @@
     
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        
+        </Fragment>
+    );
 
-    </body>
-</html>
+}    
