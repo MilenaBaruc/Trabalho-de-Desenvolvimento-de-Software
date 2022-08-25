3ea1acf0de7ddccb2352815e6326de35a1039867
Moonfih <victorhugorodriguesguimaraes@gmail.com>
2022-08-25 15:46:12 -0300

    Baixei o react ai deu ruim

--- Estudr's/front/src/App.js
+++ Estudr's/front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './assets/Logo Girassol.png';
 import { Fragment } from 'react';
 import './App.css';
@@ -9,13 +9,33 @@ import Bar from './components/Barra lateral/index.js';
 import Materias from './pages/materias/index.js';
 import Home from './pages/Home/home.js';
 
-function App() {
-  return (
-    <Fragment>
-      <Home/>
-      <Bar/>   
-    </Fragment>
-  );
+class app extends Component {
+    constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+    }
+    callAPI() {
+      fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+        .catch(err => err);
+      }
+
+    componentDidMount() {
+      this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <header className="App-header"> 
+                <img src={ logo } className="logo" alt="logo"/>
+                <h1 className="app title">Welcome to react</h1>
+              </header>
+              <p className="App-intro">{this.state.apiResponse}</p>
+            </div>
+        );
+      }
 }
 
 export default App;
