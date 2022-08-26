5e823306eddd32b04f22ee9ae6ce1e34f8c646a2
Maria Alice <marialice3003@gmail.com>
2022-08-25 22:15:44 -0300

    .gitignore

--- Estudr's/front/src/App.js
+++ Estudr's/front/src/App.js
@@ -8,34 +8,40 @@ import Config from './pages/Configuracoes/config.js';
 import Bar from './components/Barra lateral/index.js';
 import Materias from './pages/materias/index.js';
 import Home from './pages/Home/home.js';
+import LoginCad from './pages/Cadastro-Login/login-cadastro';
 
-class app extends Component {
+class App extends Component {
     constructor(props) {
     super(props);
     this.state = { apiResponse: "" };
-    }
-    callAPI() {
-      fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-        .catch(err => err);
-      }
+}
+callAPI() {
+  fetch("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({ apiResponse: res }));
+}
 
-    componentDidMount() {
-      this.callAPI();
-    }
+componentDidMount() {
+  this.callAPI();
+}
 
-    render() {
-        return (
-            <div className="App">
-              <header className="App-header"> 
-                <img src={ logo } className="logo" alt="logo"/>
-                <h1 className="app title">Welcome to react</h1>
-              </header>
-              <p className="App-intro">{this.state.apiResponse}</p>
-            </div>
-        );
-      }
+render() {
+    return (
+        <div className="App"> {/*
+          <header className="App-header"> 
+            <img src={ logo } className="logo" alt="logo"/>
+            <h1 className="app title">Welcome to react</h1>
+          </header>
+    <p className="App-intro">{this.state.apiResponse}</p> */}
+
+          <Fragment>
+            <Home/>
+            <Bar/>
+          </Fragment>
+        </div>
+
+    );
+  }
 }
 
 export default App;
