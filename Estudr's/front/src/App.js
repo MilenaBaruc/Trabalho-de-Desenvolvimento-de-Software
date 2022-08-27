c51187cf669d3307b9f914df4861acb324194202
Maria Alice <marialice3003@gmail.com>
2022-08-27 11:15:59 -0300

    Troquei as pastas

--- Estudr's/front/src/App.js
+++ Estudr's/front/src/App.js
@@ -1,47 +1,35 @@
-import React, { Component } from 'react';
-import logo from './assets/Logo Girassol.png';
-import { Fragment } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 import Config from './pages/Configuracoes/config.js';
-import Bar from './components/Barra lateral/index.js';
 import Materias from './pages/materias/index.js';
 import Home from './pages/Home/home.js';
-import LoginCad from './pages/Cadastro-Login/login-cadastro';
 
 class App extends Component {
     constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-}
-callAPI() {
-  fetch("http://localhost:9000/testAPI")
-    .then(res => res.text())
-    .then(res => this.setState({ apiResponse: res }));
-}
+        super(props);
+        this.state = { apiResponse: "" };
+    }
 
-componentDidMount() {
-  this.callAPI();
-}
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
 
-render() {
-    return (
-        <div className="App"> {/*
-          <header className="App-header"> 
-            <img src={ logo } className="logo" alt="logo"/>
-            <h1 className="app title">Welcome to react</h1>
-          </header>
-    <p className="App-intro">{this.state.apiResponse}</p> */}
+    componentDidMount() {
+        this.callAPI();
+    }
 
-          <Fragment>
-            <Home/>
-            <Bar/>
-          </Fragment>
-        </div>
-
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Home/>
+            </div>
+        );
+    }
 }
 
 export default App;
