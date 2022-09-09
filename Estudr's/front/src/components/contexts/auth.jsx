09928cee65573c1beeb13745343b46f13c1cb517
Maria Alice <marialice3003@gmail.com>
2022-09-08 21:37:29 -0300

    Auth

--- Estudr's/front/src/components/contexts/auth.jsx
+++ Estudr's/front/src/components/contexts/auth.jsx
@@ -1,18 +1,27 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext,} from "react";
+
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    
+export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     
     const login = (email, password) => {
+
         console.log("login auth", { email, password });
-        setUser({ id:"123", email:""})
+        
+        if(password == "secret"){
+            setUser({ id: "123", email });
+            navigate("/home");
+        }
     }
 
     const logout = () => {
         console.log("logout");
+        setUser(null);
+        navigate("/");
     };
 
     return(
