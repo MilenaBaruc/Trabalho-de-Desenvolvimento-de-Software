e9fe3a1c2aad9d77c9ed7178dc69c5087c8ad523
LuisMiranda10 <luiseduardocarneiro6@gmail.com>
2022-09-10 18:09:42 -0300

    commit q me mandaram fazer
    commit

---
+++ Estudr's/front/src/pages/Página de eventos/Eventos.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from "react";
+import "./styles.css";
+
+export default function PaginaEvento() {
+    return(
+        <Fragment>
+            
+        </Fragment>
+    );
+}
