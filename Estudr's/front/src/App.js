9f88e7b673c793d5f1887c785ff16bdf815a4c6f
Moonfih <victorhugorodriguesguimaraes@gmail.com>
2022-09-11 00:20:38 -0300

    Configuracoes funcionando

--- Estudr's/front/src/App.js
+++ Estudr's/front/src/App.js
@@ -6,11 +6,11 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom
 //import Cad from './pages/Cadastro/cad';
 //import Log from './pages/Login/log';
 //import Home from './pages/Home/home.js';
-//import Config from './pages/Configuracoes/config.js';
+import Config from './pages/Configuracoes/config.js';
 //import Materias from './pages/materias/index.js';
 //import Secundaria from './pages/materias-sec/Secundaria';
 //import Cad from './pages/Cadastro/cad';
-import PaginaEvento from "./pages/Página de eventos/Eventos";
+//import PaginaEvento from "./pages/Página de eventos/Eventos";
 
 import { AuthProvider, AuthContext } from "./components/contexts/auth";
 import { MdPassword } from "react-icons/md";
@@ -36,7 +36,7 @@ function App() {
                 <AuthProvider>
                     <Routes>
                  
-                    <Route exact path="/" element={<PaginaEvento/>} />
+                    <Route exact path="/" element={<Config/>} />
 
                     </Routes>
                 </AuthProvider>
---
+++ Estudr's/front/src/pages/Configuracoes/config.js
@@ -0,0 +1,44 @@
+import React, {Fragment} from "react";
+
+import "./config.css";
+import perfil from '../../assets/ImagensPerfil/verde escuro.jpg'
+import Bar from "../../components/sidebar/sidebar.js"
+
+
+
+export default function Config(){
+
+    return(
+        <Fragment>
+            <Bar/>
+            <div className="config-main" >
+                <div className="config-perfil">
+                    <img className="config-perfil-image" src={perfil} alt="user-icon" width="200px" height="200px"></img>
+                    <h1 className="config-h1">Nome Sobrenome</h1>
+                    <h3 className="config-h3">email@email.com</h3>
+                </div>
+
+                <div className="config-box">
+                    <h2 className="config-h2">Alterar senha</h2>
+                    
+                    <input className="config-input-box" type="password" placeholder="senha atual" required maxLength="8" size="8" minLength="4"></input>
+                    <input className="config-input-box" type="password" placeholder="nova senha" required maxLength="8" size="8" minLength="4"></input>
+                    <input className="config-input-box" type="password" placeholder="repita a nova senha" required maxLength="8" size="8" minLength="4"></input>
+                
+                    <button className="config-btn-save" type="submit">Salvar alterações</button>
+                </div>
+
+                <div>
+                <ul className="config-message">
+                        <li className="config-li">Sua senha deve conter no mínimo 4 carecteres e no máximo 8</li>
+                        <li className="config-li">Para maior segurança, use números e letras</li>
+                        <li className="config-li">Não compartilhe sua senha, não nos responsabilizamos por perda de conta</li>
+                </ul>
+                </div>
+
+                <button className="config-btn-delete">Excluir conta</button>
+
+            </div>
+        </Fragment>
+    );
+}
